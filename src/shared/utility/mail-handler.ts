405de50a9a8caf33286212a1160b4d6dd15b8ef0
Efren Huaman <efrenhuamanreyna@gmail.com>
2024-10-19 22:43:52 -0500

    1:30

---
+++ src/shared/utility/mail-handler.ts
@@ -0,0 +1,45 @@
+/* eslint-disable prettier/prettier */
+
+import FormData from "form-data";
+import config from 'config';
+import axios from "axios";
+
+
+export const sendEmail = async(
+    to: string,
+    templateName: string,
+    subject: string,
+    templateVars: Record<string, any> = {},
+) => {
+    try {
+        const form = new FormData();
+    form.append('to', to);
+    form.append('template', templateName);
+    form.append('subject', subject);
+    form.append(
+        'from',
+         'mailgun@sandbox737df8377dd44d7ab5d4ad340942e8c9.mailgun.org'
+        );
+    Object.keys(templateVars).forEach((key) => {
+        form.append(`v:${key}`, templateVars[key]);
+    });
+
+    const username = 'api';
+    const password = config.get('emailService.privateApiKey');
+    const token = Buffer.from(`${username}:${password}`).toString('base64');
+
+    const response = await axios({
+        method: 'post',
+        url: `https://api.mailgun.net/v3/${config.get('emailService.testDomain')}/messages`,
+        headers:{
+            Autorization: `Basic ${token}`,
+            contentType: 'multipart/form-data',
+        },
+        data:form,
+    });    
+    return response;
+    } catch (error) {
+        console.error(error);
+    }
+    
+};
